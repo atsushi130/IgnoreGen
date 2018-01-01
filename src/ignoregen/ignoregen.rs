9214f4ef956c2bfe56ad1b60a574b72dc1705e1e
atsushi130 <atsushi_main@outlook.jp>
2018-01-01 18:07:21 +0900

    change http client to hyper from curl

--- src/ignoregen/ignoregen.rs
+++ src/ignoregen/ignoregen.rs
@@ -1,35 +1,31 @@
 
 // Copyright (c) 2017 Atsushi Miyake
 //
-// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or http://apache.org/licenses/LICENSE-2.0>
+// or the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // This file may not be copied, modified, or distributed except according to those terms.
 
-use super::super::curl::easy::Easy;
-use std::io::{self, Write};
+use super::super::https_client::HttpsClientBuilder;
+use std::io::Read;
 use super::Languages;
 
 pub struct IgnoreGen;
 impl IgnoreGen {
     pub fn generate(&self, languages: Languages) -> String {
-        let mut easy  = Easy::new();
-        let query     = &languages.join();
+
+        let mut response = String::new();
+
         let base_url  = "https://www.gitignore.io/api/";
-        let url       = &self.build_url(base_url, query);
+        let url = self.build_url(base_url, &languages.join());
 
-        let mut gitignore = Vec::new();
+        HttpsClientBuilder::build()
+            .get(&url)
+            .send()
+            .unwrap()
+            .read_to_string(&mut response)
+            .unwrap();
 
-        easy.url(url).unwrap();
-
-        {
-            let mut transfer = easy.transfer();
-            transfer.write_function(|data| {
-                gitignore.extend_from_slice(data);
-                Ok(io::stdout().write(data).unwrap())
-            }).unwrap();
-            transfer.perform().unwrap();
-        }
-
-        String::from_utf8(gitignore).unwrap()
+        response
     }
 
     fn build_url(&self, base_url: &str, query: &str) -> String {
