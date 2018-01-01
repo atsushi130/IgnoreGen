16e7c6f8d8e7254309fdc04f840d4f063e1b6a3e
atsushi130 <atsushi_main@outlook.jp>
2018-01-01 18:07:49 +0900

    change license and crate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 
 // Copyright (c) 2017 Atsushi Miyake
 //
-// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or http://apache.org/licenses/LICENSE-2.0>
+// or the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // This file may not be copied, modified, or distributed except according to those terms.
 
-extern crate curl;
+extern crate hyper;
+extern crate hyper_native_tls;
 
 use std::env;
 
@@ -15,7 +17,10 @@ use ignoregen::Languages;
 mod file_manager;
 use file_manager::FileManager;
 
+mod https_client;
+
 fn main() {
+
     let args: Vec<String> = env::args().collect();
 
     if args[1..].to_vec().is_empty() {
